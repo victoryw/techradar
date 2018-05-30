709ac598428ebf09eefd24be72c0bf31de772907
tianyawy <tianya_wy@hotmail.com>
2018-05-31 01:27:08 +0800

    N/A add the httpclient

--- radars/radarData.js
+++ radars/radarData.js
@@ -134,7 +134,8 @@ var radar_data = [
             {"name":"junit4", "pc":{"r":340,"t":290},"movement":"c"},
             {"name":"驭缘支付", "pc":{"r":195,"t":310},"movement":"c"},
             {"name":"个推", "pc":{"r":120,"t":350},"movement":"c"},
-            {"name":"Knex", "pc":{"r":90,"t":330},"movement":"c"}
+            {"name":"Knex", "pc":{"r":90,"t":330},"movement":"c"},
+            {"name":"Apache HttpClient", "pc":{"r":320,"t":330},"movement":"c"}
         ]
     }
 ];
